894954effe002ac469e3c9bd12548cd710b2e913
Aarav Singh <aaravzen@gmail.com>
2022-01-23 02:03:35 -0500

    added correct behavior for words with multiples of the same letter

--- solver.py
+++ solver.py
@@ -26,11 +26,20 @@ def calculate_refreshed_yellow_words(word_list, greens):
     word_value_tuples.sort(key=lambda x: x[1], reverse=True)
     return word_value_tuples
 
-def word_finder(word_list, greens, yellows, reds):
+def word_finder(word_list, greens, yellows, reds, singles, multiples):
     ret = []
     for word in word_list:
         good = True
         remaining_yellows = yellows[:]
+
+        for letter in multiples:
+            if word.count(letter) < 2:
+                good = False
+        
+        for letter in singles:
+            if word.count(letter) != 1:
+                good = False
+
         for idx,c in enumerate(word):
             if greens[idx] != "-" and greens[idx] != c:
                 good = False
@@ -47,34 +56,46 @@ def word_finder(word_list, greens, yellows, reds):
                 break
 
             remaining_yellows = [x for x in remaining_yellows if x[0] != c]
+        
         if good and not len(remaining_yellows):
             ret.append(word)
     return ret
 
-def calculate_new_colors(guess,response,greens,yellows,reds,good_letters):
+def calculate_new_colors(guess,response,greens,yellows,reds,singles,multiples):
     new_greens = ""
+    good_letters = set()
     for idx,color in enumerate(response):
         if color.upper() == "G":
             new_greens += guess[idx]
+            if guess[idx] in good_letters:
+                multiples.add(guess[idx])
             good_letters.add(guess[idx])
         elif color.upper() == "Y":
             yellows.append((guess[idx],idx))
             new_greens += greens[idx]
+            if guess[idx] in good_letters:
+                multiples.add(guess[idx])
             good_letters.add(guess[idx])
         else:
+            new_greens += greens[idx]
+    
+    for idx,color in enumerate(response):
+        if color.upper() != "G" and color.upper() != "Y":
             if guess[idx] not in good_letters:
                 reds += guess[idx]
-            new_greens += greens[idx]
-    return new_greens,yellows,reds,good_letters
+            else:
+                singles.add(guess[idx])
+    return new_greens,reds
 
 
 def main():
     greens = "-----"
     yellows = []
     reds = ""
-    good_letters = set()
+    confirmed_singles = set()
+    confirmed_multiples = set()
     word_list = get_sorted_best_word_list()
-    found = word_finder(word_list, greens, yellows, reds)
+    found = word_finder(word_list, greens, yellows, reds, confirmed_singles, confirmed_multiples)
     guess = found[0]
     print(f"Hi, welcome to your personalized wordle solver! Start with {guess}")
     print("What does that give you?")
@@ -82,11 +103,13 @@ def main():
     wordle_round = 1
     while user_answer.upper() != "GGGGG" and wordle_round < 6:
         wordle_round += 1
-        greens,yellows,reds,good_letters = calculate_new_colors(guess, user_answer, greens, yellows, reds, good_letters)
+        greens,reds = calculate_new_colors(guess, user_answer, greens, yellows, reds, confirmed_singles, confirmed_multiples)
         print(f"\ngreens: {greens}")
         print(f"yellows: {yellows}")
-        print(f"reds: {reds}\n")
-        found = word_finder(word_list, greens, yellows, reds)
+        print(f"reds: {reds}")
+        print(f"singles: {confirmed_singles}")
+        print(f"multiples: {confirmed_multiples}\n")
+        found = word_finder(word_list, greens, yellows, reds, confirmed_singles, confirmed_multiples)
         found = get_sorted_best_word_list(calculate_refreshed_yellow_words(found, greens))
         guess = found[0]
         print(f"These are you options now: {', '.join(w for w in found)}")
@@ -105,8 +128,8 @@ def main():
     if user_answer.upper() == "GGGGG":
         print(f"Yay! you just got the wordle in {wordle_round} rounds!")
     else:
-        greens,yellows,reds,good_letters = calculate_new_colors(guess, user_answer, greens, yellows, reds, good_letters)
-        found = word_finder(word_list, greens, yellows, reds)
+        greens,reds = calculate_new_colors(guess, user_answer, greens, yellows, reds, confirmed_singles, confirmed_multiples)
+        found = word_finder(word_list, greens, yellows, reds, confirmed_singles, confirmed_multiples)
         print("I'm sorry you didn't get the wordle :( The other possible words are:")
         print("\n".join(word for word in found))
     
